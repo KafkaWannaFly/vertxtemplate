394730930603af4e644936d7ad4cc8f976941e05
KafkaUnderCurrent <kafka@undercurrent.tech>
2025-06-22 00:34:14 +0700

    Fix snake_case name mapping

--- src/main/java/vertxtemplate/models/responses/Film.java
+++ src/main/java/vertxtemplate/models/responses/Film.java
@@ -1,12 +1,13 @@
 package vertxtemplate.models.responses;
 
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.format.SnakeCase;
 import io.vertx.sqlclient.templates.annotations.RowMapped;
 import lombok.Data;
 
 @Data
 @DataObject
-@RowMapped
+@RowMapped(formatter = SnakeCase.class)
 public class Film {
     private Integer id;
     private String title;
